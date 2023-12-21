e4613ef5bd942197551947c59c942f76013730ab
ZzinB <mysteleee7@gmail.com>
2023-12-21 19:29:03 +0900

    Add : 추가 메서드매핑

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.requestmapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,9 @@ public class MappingController {
     /**
      * 기본 요청
      * HTTP 메서드 모두 허용 GET, HEAD, POST, PUT, PATCH, DELETE */
-    @RequestMapping("/hello-basic")
+//    @RequestMapping("/hello-basic")
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
+
     public String helloBasic(){
         log.info("helloBasic");
         return "ok";
