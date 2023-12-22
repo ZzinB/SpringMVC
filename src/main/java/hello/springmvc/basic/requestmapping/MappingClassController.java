a2c560ff3a3e4a368183302e9ca85d6dabf1d0bf
ZzinB <mysteleee7@gmail.com>
2023-12-22 17:22:59 +0900

    Refact:  리팩토링 매핑예제

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.requestmapping;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/mapping/users")
 public class MappingClassController {
     /**
      * 회원 목록 조회: GET            /users
@@ -13,23 +14,23 @@ public class MappingClassController {
      * @return
      */
 
-    @GetMapping("/mapping/users")
+    @GetMapping
     public  String user(){
         return "get users";
     }
-    @PostMapping("/mapping/users")
+    @PostMapping
     public String addUser(){
         return "post user";
     }
-    @GetMapping("/mapping/users/{userId}")
+    @GetMapping("/{userId}")
     public String findUser(@PathVariable String userId){
         return "get userId= " + userId;
     }
-    @PatchMapping("/mapping/users/{userId}")
+    @PatchMapping("/{userId}")
     public String updateUser(@PathVariable String userId){
         return "update userId= " + userId;
     }
-    @DeleteMapping("/mapping/users/{userId}")
+    @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable String userId){
         return "delete userId= " + userId;
     }
