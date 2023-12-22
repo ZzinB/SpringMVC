ae9a88ef8998a90901703cd4c0c5e5177bbb4c46
ZzinB <mysteleee7@gmail.com>
2023-12-22 21:42:01 +0900

    Comment : 추가 주석설명

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -5,6 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,6 +56,12 @@ public class RequestBodyStringController {
         log.info("messageBody={}", messageBody);
         return new HttpEntity<>("ok");
     }
+//    @PostMapping("/request-body-string-v3")
+//    public HttpEntity<String> requestBodyStringV3(RequestEntity<String> httpEntity) throws IOException{
+//        String messageBody = httpEntity.getBody();
+//        log.info("messageBody={}", messageBody);
+//        return new ResponseEntity<>("ok", HttpStatus.CREATED);
+//    }
 
     /**
      * @RequestBody
