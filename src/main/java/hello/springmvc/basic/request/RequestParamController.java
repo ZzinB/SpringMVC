361829b8029cbcec56592e5272be032aa644b0d7
ZzinB <mysteleee7@gmail.com>
2023-12-22 18:34:21 +0900

    Add : 추가 파라미터 필수 여부

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -61,4 +61,13 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
         return "ok";
     }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) int age){
+        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
 }
